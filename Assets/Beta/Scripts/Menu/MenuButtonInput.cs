1a9aa67ea95af2e8b4f5ab24ed67b0168af45226
TsuyoshiUsugi <qesulive@gmail.com>
2022-10-22 23:10:55 +0900

    [update] Menu画面のボタン入力を読み取るスクリプトを用意

---
+++ Assets/Beta/Scripts/Menu/MenuButtonInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// メニュー画面の入力を読み取るスクリプト
+/// </summary>
+public class MenuButtonInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
