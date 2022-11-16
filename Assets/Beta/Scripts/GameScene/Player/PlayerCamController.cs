1c9fbbda7810aa269f92bc05a0e6ec0380fdf511
TsuyoshiUsugi <qesulive@gmail.com>
2022-11-17 03:05:34 +0900

    [update] プレイヤーカメラの動きができた

--- Assets/Beta/Scripts/GameScene/Player/PlayerCamController.cs
+++ Assets/Beta/Scripts/GameScene/Player/PlayerCamController.cs
@@ -13,10 +13,26 @@ public class PlayerCamController : MonoBehaviour
 
     [SerializeField] float _camSpeed;
     [SerializeField] Vector2 _camDir;
+    [SerializeField] float _max;
+    [SerializeField] float _min;
 
     float _camX = 0;
     float _camY = 0;
-    // Update is called once per frame
+    Quaternion _quaternionMax;
+    Quaternion _quaternionMin;
+
+
+    private void Start()
+    {
+        EulerToQuaternion();
+    }
+
+    private void EulerToQuaternion()
+    {
+        _quaternionMax = Quaternion.Euler(_max, 0, 0);
+        _quaternionMin = Quaternion.Euler(_min, 0, 0);
+    }
+
     void Update()
     {
         ReadInput();
@@ -43,5 +59,15 @@ public class PlayerCamController : MonoBehaviour
 
         this.transform.rotation = Quaternion.AngleAxis(_camX, Vector3.up);
         _eye.transform.localRotation = Quaternion.AngleAxis(_camY, Vector3.right);
+
+
+        if (_eye.transform.localRotation.x > _quaternionMax.x)
+        {
+            _eye.transform.localRotation = _quaternionMax;
+        }
+        else if (_eye.transform.localRotation.x < _quaternionMin.x)
+        {
+            _eye.transform.localRotation = _quaternionMin;
+        }
     }
 }
