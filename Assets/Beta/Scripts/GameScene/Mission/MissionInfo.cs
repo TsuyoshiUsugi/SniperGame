d6d29ba4a49a78a88d055bed727929e89e044c77
TsuyoshiUsugi <qesulive@gmail.com>
2022-12-09 10:04:46 +0900

    [update] MissionInfoの仮の型完成、敵AIの作成に移る

--- Assets/Beta/Scripts/GameScene/Mission/MissionInfo.cs
+++ Assets/Beta/Scripts/GameScene/Mission/MissionInfo.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// ミッション情報が記載されたスクリプタブルオブジェクト
@@ -16,11 +17,48 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="MissionInfo", menuName = "ScriptableObjects/MissionInfo")]
 public class MissionInfo : ScriptableObject
 {
-
+    
     [Header("ミッション名")]
     [SerializeField] public string MissionName;
+    public List<TargetInfo> _targetInfos;
+    public EnemyInfo _enemyInfo;
+    public StageInfo _stageInfo;
 
-    [Header("ターゲット情報")]
-    [SerializeField] public string TargetName;
+    /// <summary>
+    /// ターゲット情報まとめたクラス
+    /// </summary>
+    [Serializable]
+    public class TargetInfo
+    {
+        [SerializeField] string _targetName;
+        [SerializeField] Sprite _targetImage;
+        [SerializeField] string _targetInfo;
+        [SerializeField] Vector3 _targetAI; //ここは独自のAIクラスにする予定
+        //モデルの参照を書く
+    }
+
+    /// <summary>
+    /// ザコ敵の情報をまとめたクラス
+    /// </summary>
+    [Serializable]
+    public class EnemyInfo
+    {
+        [SerializeField] int _targetNum;
+        //独自のAIクラスの変数を用意
+    　　//参照も書く
+    }
+
+    /// <summary>
+    /// ステージ情報をまとめたクラス
+    /// </summary>
+    [Serializable]
+    public class StageInfo
+    {
+        [SerializeField] int _stageIndex;
+        [SerializeField] int _time;
+        [SerializeField] float _windDirection;
+    }
+        
+
 
 }
