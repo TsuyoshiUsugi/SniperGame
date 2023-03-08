2ec055e0ac82722c650d3a133647c71c3d2a929b
TsuyoshiUsugi <qesulive@gmail.com>
2023-03-08 09:02:18 +0900

    [add] 新しいカメラスクリプト作成

---
+++ Assets/Beta/Scripts/GameScene/Player/newPlayerControle/PlayerCamControlManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerCamControlManager : MonoBehaviour
+{
+    [Header("éQè∆")]
+    [SerializeField] GameObject _player;
+    [SerializeField] InputManager _inputManager;
+
+    [SerializeField] AxisState _rotateX;
+    [SerializeField] AxisState _rotateY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+
+        Rotate();
+    }
+
+    /// <summary>
+    /// ì¸óÕÇì«Ç›éÊÇÈ
+    /// </summary>
+    private void ReadInput()
+    {
+        
+    }
+
+    void Rotate()
+    {
+        
+    }
+}
