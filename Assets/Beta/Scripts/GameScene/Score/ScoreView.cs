3de639aac672b22671250324ce21370b4e441b7e
TsuyoshiUsugi <qesulive@gmail.com>
2023-03-09 17:37:26 +0900

    [update] リザルトにアニメーションを追加

--- Assets/Beta/Scripts/GameScene/Score/ScoreView.cs
+++ Assets/Beta/Scripts/GameScene/Score/ScoreView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 /// <summary>
 /// スコアを表示する
@@ -24,16 +25,27 @@ public class ScoreView : MonoBehaviour
     [SerializeField] Text _scoreText;
     [SerializeField] string _nonCheck;
     [SerializeField] Text _rankText;
+    [SerializeField] List<Text> _rightTexts;
+    [SerializeField] List<Text> _midleTexts;
+    [SerializeField] List<Text> _leftTexts;
+
+    float _showDelay = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         Initialize();
+        
     }
 
     private void Initialize()
     {
         _resultObj.SetActive(false);
+        _leftTexts.ForEach(text => text.DOFade(0, 0));
+        _midleTexts.ForEach(text => text.DOFade(0, 0));
+        _rightTexts.ForEach(text => text.DOFade(0, 0));
+        _rankText.gameObject.SetActive(false);
+
     }
 
     public void SetScoreView()
@@ -46,7 +58,7 @@ public class ScoreView : MonoBehaviour
             _failText.text = _nonCheck;
             _ClearScoreText.text = ScoreManager.Instance.ClearScore.ToString();
         }
-        
+
         //ミッション失敗
         if (ScoreManager.Instance.MissionFailed)
         {
@@ -59,7 +71,7 @@ public class ScoreView : MonoBehaviour
         _timeScoreText.text = ScoreManager.Instance.TimeScore.ToString();
 
         //非検知
-        if(ScoreManager.Instance.IsDetected)
+        if (ScoreManager.Instance.IsDetected)
         {
             _isDetectedScoreText.text = ScoreManager.Instance.DetectedScore.ToString();
         }
@@ -79,5 +91,43 @@ public class ScoreView : MonoBehaviour
         //ランク
         _rankText.text = ScoreManager.Instance.ScoreRank();
 
+        StartCoroutine(nameof(ShowResult));
+    }
+
+    IEnumerator ShowResult()
+    {
+        var endValue = 38;
+        var startValue = 622;
+        var dur = 1f;
+
+        var resultObjRect = _resultObj.GetComponent<RectTransform>();
+        resultObjRect.position = new(resultObjRect.position.x, startValue, resultObjRect.position.z);
+        resultObjRect.DOAnchorPosY(endValue, dur);
+
+        yield return new WaitForSeconds(_showDelay);
+
+        for (int i = 0; i < _midleTexts.Count; i++)
+        {
+            AlphaAnim(_leftTexts[i]);
+            AlphaAnim(_midleTexts[i]);
+            AlphaAnim(_rightTexts[i]);
+
+            yield return new WaitForSeconds(_showDelay);
+        }
+
+        AlphaAnim(_leftTexts[_leftTexts.Count - 1]);
+        AlphaAnim(_rightTexts[_rightTexts.Count - 1]);
+
+        yield return new WaitForSeconds(_showDelay);
+
+        _rankText.gameObject.SetActive(true);
+    }
+
+    void AlphaAnim(Text text)
+    {
+        var alpha = 1;
+        var delay = 0.25f;
+
+        DOTween.ToAlpha(() => text.color, color => text.color = color, alpha, delay);
     }
 }
